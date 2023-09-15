ea934c379e5dd40fef236151ca2d148ec354cf72
Neural-Enigma <fcad.td@gmail.com>
2023-09-15 18:35:32 +0300

    Update OrangeTrader.java

--- DatsOrangeHackathon/src/main/java/test/OrangeTrader.java
+++ DatsOrangeHackathon/src/main/java/test/OrangeTrader.java
@@ -6,15 +6,18 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
 
+
 @Service
 public class OrangeTrader {
 
     private String token = "64f38d2665df964f38d2665dfd";
     private RestTemplate restTemplate = new RestTemplate();
-    private String baseUrl = "https://datsorange.devteam.games";
+    private final String baseUrl = "https://datsorange.devteam.games";
     private long symbolId = 2; // ID апельсинов
 
     private List<Long> buyOrderIds = new ArrayList<>();
+    private List<Long> buyPrices = new ArrayList<>();
+    private int activeBids = 0;
 
     public void placeBuyOrder(long price, int quantity) {
         String url = baseUrl + "/LimitPriceBuy";
@@ -35,6 +38,8 @@ public class OrangeTrader {
         if (response.getStatusCode() == HttpStatus.OK) {
             Long buyOrderId = (Long) response.getBody().get("bidId");
             buyOrderIds.add(buyOrderId);
+            buyPrices.add(price);
+            activeBids++;
         }
     }
 
@@ -51,8 +56,20 @@ public class OrangeTrader {
         if (response.getStatusCode() == HttpStatus.OK) {
             for (Map stock : response.getBody()) {
                 long stockPrice = (Long) stock.get("price");
-                // Проверьте, есть ли возможность продать апельсины по выгодной цене
-                // здесь ваша логика
+
+                for (int i = 0; i < buyPrices.size(); i++) {
+                    long boughtPrice = buyPrices.get(i);
+
+                    if (stockPrice > boughtPrice) {
+                        placeSellOrder(stockPrice, 1);  // 1 quantity, adjust as needed
+                        cancelOrder(buyOrderIds.get(i));
+
+                        buyPrices.remove(i);
+                        buyOrderIds.remove(i);
+                        i--;  // adjust index due to removal
+                        activeBids--;
+                    }
+                }
             }
         }
     }
