fe6368a989e2969af6166eb5d7f404fb2e357a8c
Lightning-Andrey <andrejkamors@gmail.com>
2023-09-16 10:27:12 +0300

    Update CompaniesInfo.java

--- DatsOrangeHackathon/src/main/java/com/modus/DatsOrangeHackathon/CompaniesInfo.java
+++ DatsOrangeHackathon/src/main/java/com/modus/DatsOrangeHackathon/CompaniesInfo.java
@@ -1,2 +1,53 @@
-package com.modus.DatsOrangeHackathon;public class CompaniesInfo {
+package com.modus.DatsOrangeHackathon;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+import static com.modus.DatsOrangeHackathon.Const.*;
+
+public class CompaniesInfo {
+
+    public static final Map[] companies = new Map[2002];
+
+    public class Company {
+        public final int companyId;
+        public double priceFactor;
+        public final String companyName;
+
+        public Company(int companyId, String companyName) {
+            this.companyId = companyId;
+            this.priceFactor = 1;
+            this.companyName = companyName;
+        }
+
+        public void changePrice(double rate) {
+            priceFactor *= (1 + rate / 100);
+        }
+    }
+
+    public static Map[] getCompanies() throws IOException {
+        Request request = new Request.Builder()
+                .url(baseUrl + "/getSymbols")
+                .addHeader("token", TOKEN)
+                .get()
+                .build();
+        Map[] companies = new Map[2002];
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                assert response.body() != null;
+
+            }
+        }
+        return companies;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
